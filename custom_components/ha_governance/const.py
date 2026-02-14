11ee5a3d4ef5fbbffb92a46132cf34f307e8f9d5
Starsurfer78 <70095045+Starsurfer78@users.noreply.github.com>
2026-02-14 11:52:23 +0100

    Create const.py

---
+++ custom_components/ha_governance/const.py
@@ -0,0 +1,7 @@
+DOMAIN = "ha_governance"
+DATA_KEY = "data"
+CONF_COOLDOWN_SECONDS = "cooldown_seconds"
+CONF_MODE_ENTITY = "mode_entity"
+CONF_POLICY_PATH = "policy_path"
+DEFAULT_COOLDOWN_SECONDS = 10
+DEFAULT_POLICY_PATH = "/config/ha_governance/policies.yaml"
